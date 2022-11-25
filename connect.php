75e03e3e992e88961cb4cf85881ffba7d38bb3a9
Akhilesh <98794660+Saga-Akhilesh@users.noreply.github.com>
2022-11-25 23:43:53 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,7 @@
+<?php
+$hostname  = 'sql101.epizy.com';
+$username = 'epiz_33073981';
+$password='wX2BiaNbjg99E';
+$dbname = 'epiz_33073981_train';
+$connect =  mysqli_connect($hostname , $username , $password ,$dbname) or die("Error Connecting");
+?>
